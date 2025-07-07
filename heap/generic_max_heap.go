872863d3c2426bf1ba11f60dded4bf79bb48d5e4
Arsham <mr.karimi@corp.mabnadp.com>
2025-07-07 19:02:48 +0330

    add: generic max heap.

---
+++ heap/generic_max_heap.go
@@ -0,0 +1,79 @@
+package heap
+
+import (
+	"fmt"
+)
+
+type CompareFunc[T any] func(a, b T) int
+
+type GMaxBinaryHeap[T any] struct {
+	compare   CompareFunc[T]
+	values    []T
+	maxLength int
+}
+
+func NewGMaxBinaryHeap[T any](compareFunc CompareFunc[T]) *GMaxBinaryHeap[T] {
+	return &GMaxBinaryHeap[T]{
+		values: make([]T, 0),
+	}
+}
+
+func (bh *GMaxBinaryHeap[T]) Push(value T) *GMaxBinaryHeap[T] {
+	current_idx := len(bh.values)
+	bh.values = append(bh.values, value)
+	parent_idx := (current_idx - 1) / 2
+
+	// Bubble Up
+	for bh.compare(bh.values[parent_idx], value) == -1 {
+		bh.values[parent_idx], bh.values[current_idx] = bh.values[current_idx], bh.values[parent_idx]
+		current_idx = parent_idx
+		parent_idx = (current_idx - 1) / 2
+	}
+	return bh
+}
+
+func (bh *GMaxBinaryHeap[T]) Pop() (value T, ok bool) {
+	if len(bh.values) == 0 {
+		return value, false
+	}
+	value = bh.values[0]
+	last_index := len(bh.values) - 1
+	bh.values[0] = bh.values[last_index]
+	bh.values = bh.values[:last_index]
+
+	// Sink down
+	n := 0
+	next_idx := bh.nextIndex(n)
+	for next_idx != 0 && bh.compare(bh.values[n], bh.values[next_idx]) == -1 {
+		bh.values[n], bh.values[next_idx] = bh.values[next_idx], bh.values[n]
+		n = next_idx
+		next_idx = bh.nextIndex(n)
+	}
+
+	return value, true
+}
+
+// Returns the index of the maximum of the next row.
+// If the current node is a leaf, returns 0
+func (bh *GMaxBinaryHeap[T]) nextIndex(n int) (idx int) {
+	idx_left := 2*n + 1
+	idx_right := idx_left + 1
+	if idx_right >= len(bh.values) {
+		if idx_left >= len(bh.values) {
+			return 0
+		}
+		return idx_left
+	}
+	if bh.compare(bh.values[idx_left], bh.values[idx_right]) == 1 {
+		return idx_left
+	}
+	return idx_right
+}
+
+func (bh *GMaxBinaryHeap[T]) String() string {
+	return fmt.Sprint(bh.values)
+}
+
+func (bh *GMaxBinaryHeap[T]) Len() int {
+	return len(bh.values)
+}
